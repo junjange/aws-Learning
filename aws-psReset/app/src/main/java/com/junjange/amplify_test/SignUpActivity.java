214fdf04ca3ee12b12cd92b28b591b5f27d62226
junjange <wnswkd486@naver.com>
2021-07-18 20:49:53 +0900

    aws-psReset
    비밀번호 재설정 기능 추가

---
+++ aws-psReset/app/src/main/java/com/junjange/amplify_test/SignUpActivity.java
@@ -0,0 +1,99 @@
+package com.junjange.amplify_test;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.Callback;
+import com.amazonaws.mobile.client.results.SignUpResult;
+import com.amazonaws.mobile.client.results.UserCodeDeliveryDetails;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignUpActivity extends AppCompatActivity {
+    String TAG = AuthActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+        Button signUp_button2 = findViewById(R.id.signUp_button2); // 회원 가입 버튼
+
+        // 회원 가입 버튼
+        signUp_button2.setOnClickListener(new View.OnClickListener() {
+
+
+            // 회원가입
+            @Override
+            public void onClick(View v) {
+
+                // 이름, 아이디(이메일), 비밀번호 순
+                EditText signUpName =  (EditText)findViewById(R.id.signUpName);
+                EditText signUpUsername =  (EditText)findViewById(R.id.signUpUsername);
+                EditText signUpPassword =  (EditText)findViewById(R.id.signUpPassword);
+
+                String name = signUpName.getText().toString();
+                String username = signUpUsername.getText().toString();
+                String password = signUpPassword.getText().toString();
+
+
+                final Map<String, String> attributes = new HashMap<>();
+                attributes.put("name", name);
+                attributes.put("email", username);
+                Log.d("TTT",attributes.toString());
+
+
+                AWSMobileClient.getInstance().signUp(username, password, attributes, null, new Callback<SignUpResult>() {
+                    @Override
+                    public void onResult(final SignUpResult signUpResult) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                Log.d(TAG, "Sign-up callback state: " + signUpResult.getConfirmationState());
+                                if (!signUpResult.getConfirmationState()) {
+
+
+                                    final UserCodeDeliveryDetails details = signUpResult.getUserCodeDeliveryDetails();
+                                    Toast.makeText(getApplicationContext(), "인증 메일을 보냈습니다.: " + details.getDestination(), Toast.LENGTH_SHORT).show();
+
+                                    // 이메일에 문제가 없으면 인증 코드 창으로 이동
+                                    Intent i = new Intent(SignUpActivity.this, OkActivity.class);
+                                    i.putExtra("email",username); // username을 인증 코드 창에서 사용하기 위해
+                                    startActivity(i);
+                                    finish();
+
+                                } else {
+                                    // 인증 코드 창으로 이동
+                                    Toast.makeText(getApplicationContext(), "Sign-up done.", Toast.LENGTH_SHORT).show();
+                                    Log.e(TAG, "Sddd");
+                                }
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Log.e(TAG, "Sign-up error", e);
+                    }
+                });
+            }
+
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+            Intent i = new Intent(SignUpActivity.this, AuthActivity.class);
+            startActivity(i);
+            finish();
+    }
+}
